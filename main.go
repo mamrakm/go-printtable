94ca3cf1c520896db022814c4422fa58dc429e86
Miroslav Mamrak <miroslav.mamrak@xura.com>
2017-06-15 18:40:36 +0200

    Users table

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -12,8 +14,43 @@ var arr = []string{
 	"George", "Michael", "Robert", "Dennis", "Ian",
 }
 
+//Users struct contains data about users from /etc/passwd
+type Users struct {
+	uid     int
+	gid     int
+	name    string
+	homedir string
+}
+
+func getUsersList() []Users {
+	users := make([]Users, 32)
+	pwd, err := os.Open("/etc/passwd")
+	if err != nil {
+		log.Println("Unable to open /etc/passwd: ", err)
+	}
+	buff := bufio.NewReader(pwd)
+	for {
+		line, err := buff.ReadString('\n')
+		if err != nil {
+			break
+		}
+		data := strings.Split(line, ":")
+		uid, err := strconv.Atoi(data[2])
+		if err != nil {
+			log.Fatal("Unable to read UID: ", err)
+		}
+		gid, err := strconv.Atoi(data[3])
+		if err != nil {
+			log.Fatal("Unable to read GID: ", err)
+		}
+		users = append(users, Users{uid: uid, gid: gid, name: data[0], homedir: data[5]})
+	}
+	return users
+}
+
 func main() {
-	fmt.Println(printBoxes(arr, maxArrayLen(arr)))
+	res := getUsersList()
+	fmt.Println(printUsers(res))
 }
 
 func printArr(in []string) string {
@@ -38,15 +75,28 @@ func maxArrayLen(in []string) (max int) {
 	return max
 }
 
-func printBoxes(s []string, i int) string {
+func printUsers(s []Users) string {
 	var strbuff string
 	ret := bytes.NewBufferString(strbuff)
-	printBox(ret, i)
-	for _, line := range s {
-		pad := strings.Repeat(" ", i-len(line))
-		ret.WriteString("|" + line + pad)
-		ret.WriteString("|\n")
-		printBox(ret, i)
+	for _, user := range s {
+		if user.name == "" {
+			continue
+		}
+		u := []string{
+			strconv.Itoa(user.uid),
+			strconv.Itoa(user.gid),
+			user.name,
+			user.homedir,
+		}
+		l := maxArrayLen(u)
+		printBox(ret, 4*l+3)
+		ret.WriteString("|")
+		for _, line := range u {
+			pad := strings.Repeat(" ", l-len(line))
+			ret.WriteString(line + pad + "|")
+		}
+		ret.WriteString("\n")
+		printBox(ret, 4*l+3)
 	}
 	return ret.String()
 }
