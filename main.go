392d1cd8c35753803ab8c0ea778d18a09be9901c
nehalem <1453constantinopole@gmail.com>
2017-06-15 20:22:55 +0200

    Polish

--- main.go
+++ main.go
@@ -10,16 +10,16 @@ import (
 	"strings"
 )
 
-//Users struct contains data about users from /etc/passwd
-type Users struct {
+//users struct contains data about users from /etc/passwd
+type users struct {
 	uid     int
 	gid     int
 	name    string
 	homedir string
 }
 
-func getUsersList() []Users {
-	users := make([]Users, 32)
+func getUsersList() []users {
+	usr := make([]users, 32)
 	pwd, err := os.Open("/etc/passwd")
 	defer pwd.Close()
 	if err != nil {
@@ -40,26 +40,14 @@ func getUsersList() []Users {
 		if err != nil {
 			log.Fatal("Unable to read GID: ", err)
 		}
-		users = append(users, Users{uid: uid, gid: gid, name: data[0], homedir: data[5]})
+		usr = append(usr, users{uid: uid, gid: gid, name: data[0], homedir: data[5]})
 	}
-	return users
+	return usr
 }
 
 func main() {
 	res := getUsersList()
 	fmt.Println(printUsers(res))
-}
-
-func printArr(in []string) string {
-	max := maxArrayLen(in)
-	var ret string
-	buf := bytes.NewBufferString(ret)
-	str := strconv.Itoa(max)
-	_, err := buf.WriteString(str)
-	if err != nil {
-		log.Println("Unable to write string: ", err)
-	}
-	return buf.String()
 }
 
 func maxArrayLen(in []string) (max int) {
@@ -72,7 +60,7 @@ func maxArrayLen(in []string) (max int) {
 	return max
 }
 
-func printUsers(s []Users) string {
+func printUsers(s []users) string {
 	var strbuff string
 	ret := bytes.NewBufferString(strbuff)
 	for _, user := range s {
@@ -86,19 +74,19 @@ func printUsers(s []Users) string {
 			user.homedir,
 		}
 		l := maxArrayLen(u)
-		printBox(ret, 4*l+3)
+		printLineSeparator(ret, 4*l+3)
 		ret.WriteString("|")
 		for _, line := range u {
 			pad := strings.Repeat(" ", l-len(line))
 			ret.WriteString(line + pad + "|")
 		}
 		ret.WriteString("\n")
-		printBox(ret, 4*l+3)
+		printLineSeparator(ret, 4*l+3)
 	}
 	return ret.String()
 }
 
-func printBox(buff *bytes.Buffer, i int) string {
+func printLineSeparator(buff *bytes.Buffer, i int) string {
 	buff.WriteString("+")
 	for l := 0; l < i; l++ {
 		buff.WriteString("-")
